0f819320fdd302397743971ea9acf7523529363c
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2023-06-03 00:07:30 +0530

    Initial commit

---
+++ DataSense/DataSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataSenseApp.swift
+//  DataSense
+//
+//  Created by Ganesh Raman on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DataSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
